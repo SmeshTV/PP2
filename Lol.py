46a4357a9a79a9ae175d03005ef49182403d4197
SmeshTV <armannrmakhanbetov@gmail.com>
2025-01-14 15:10:28 +0500

    Edited Lol.py

--- Lol.py
+++ Lol.py
@@ -1,2 +1,14 @@
-a = 3+3
-print(a)
+print("Hello, World!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+x = 5
+y = "John"
+print(x, y)
+
+import random
+
+print(random.randrange(1, 10))
